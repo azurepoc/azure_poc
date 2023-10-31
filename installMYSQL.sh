f6bfaad60cff673d006fc07bbe2b64abb7145d7f
azurepoc <149358615+azurepoc@users.noreply.github.com>
2023-10-31 11:47:21 +0530

    Add files via upload

---
+++ installMYSQL.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update the package list to ensure you have the latest information
+sudo apt update
+
+# Install MySQL Server
+sudo apt install mysql-server
+
+echo "MYSQL install finished"
+
